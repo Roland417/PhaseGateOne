03ba51a3fa8a114ab4f1cd6b27142af00d52a251
Cookies <rolandchurchboy417@gmail.com>
2025-12-08 15:43:05 +0100

    first commit

---
+++ phaseGate1/JavaScript/RandomSubstraction.js
@@ -0,0 +1,20 @@
+let number1 = 100;
+let number2 = 10;
+let random = 0;
+
+for(let index = 1; index = number1 >= number2; index ++){
+
+let number1 - number2;
+}
+  
+
+
+}
+
+if("Enter questions");
+    console.log("Start solving");
+if("Second attempt");
+    console.log("Last attempt")
+if("Ended");
+    console.log("Wait for your scores!");
+
---
+++ phaseGate1/java/RandomSubstraction.java
@@ -0,0 +1,32 @@
+import random.Scanner;
+
+public class RandomSubstraction{
+public static void main(String[] args){
+
+Random random = new Random(System.in);
+System.out.println("Enter questions");
+System.out.println("Enter answer");
+
+int number = input.nextInt;
+
+String displays = Calculate(numbers);
+
+ }
+System.out.println(answer);
+}
+public class static Calculate(int number){
+
+int displayQuestions += (1, 100);
+int displayAnswers = (1, 10) / 1000;
+
+for(count = 0; count = random.randint; count--);
+
+while
+    (displayQuestions >= 100){
+    displayAnswers -= 10;
+
+int displayQusetions = displayAnswer -< 100;
+    if("Enter questions") return (random questions);
+    if(2 attempts) return ("Final attempt!")
+}
+
